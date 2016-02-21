9a8ece9a8cb5f2ca7e5c81a81d7476a8f3fb3133
Luan Nguyen Thanh <tklarryonline@gmail.com>
2016-02-21 17:22:43 +0700

    Add post page

--- aiki_blog/urls.py
+++ aiki_blog/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.PostListView.as_view(), name='post_list')
+    url(r'^$', views.PostListView.as_view(), name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='post_detail')
 ]
--- aiki_blog/views.py
+++ aiki_blog/views.py
@@ -1,3 +1,4 @@
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from .models.post import Post
@@ -6,3 +7,8 @@ from .models.post import Post
 class PostListView(ListView):
     queryset = Post.objects.order_by('-published_date')
     context_object_name = 'posts'
+
+
+class PostDetailView(DetailView):
+    context_object_name = 'post'
+    model = Post
