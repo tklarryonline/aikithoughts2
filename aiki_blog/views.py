70d04eb690fd6daeb50d9afba1a7f7ef0c798e65
Luan Nguyen Thanh <tklarryonline@gmail.com>
2016-02-21 17:35:53 +0700

    [r] Restructure post and base htmls file

--- aiki_blog/views.py
+++ aiki_blog/views.py
@@ -7,8 +7,10 @@ from .models.post import Post
 class PostListView(ListView):
     queryset = Post.objects.order_by('-published_date')
     context_object_name = 'posts'
+    template_name = 'post/list.html'
 
 
 class PostDetailView(DetailView):
     context_object_name = 'post'
     model = Post
+    template_name = 'post/detail.html'
