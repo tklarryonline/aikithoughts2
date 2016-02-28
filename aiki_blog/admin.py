c990d13ae3426a6473bff61ac6e6556c1a67866d
Luan Nguyen Thanh <tklarryonline@gmail.com>
2016-02-29 01:15:06 +0700

    [f] 13: Allow Admins to edit Posts in Markdown
    https://trello.com/c/CPeV5HLh/

--- aiki_blog/admin.py
+++ aiki_blog/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
+from django.db.models.fields import TextField
+from markdownx.widgets import AdminMarkdownxWidget
 
 from .models import Post
 
-admin.site.register(Post)
+
+class PostModelAdmin(ModelAdmin):
+    formfield_overrides = {
+        TextField: {
+            'widget': AdminMarkdownxWidget
+        },
+    }
+
+
+admin.site.register(Post, admin_class=PostModelAdmin)
