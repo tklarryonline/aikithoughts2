6decfb22245fb69da6069d5101b59beed56ffdf4
Luan Nguyen Thanh <tklarryonline@gmail.com>
2016-03-04 00:49:41 +0700

    [f] 14: Switch to use SCSS
    https://trello.com/c/o6SSSyHM

--- aikithoughts/settings/base.py
+++ aikithoughts/settings/base.py
@@ -135,6 +135,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+
     'sass_processor.finders.CssFinder',
 )
 
